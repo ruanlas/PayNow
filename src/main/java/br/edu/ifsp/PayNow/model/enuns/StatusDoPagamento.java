db81416ffd49ab78dbd1d795b551901071cd6e06
renatosrib <renatosrib@gmail.com>
2017-11-01 20:54:31 -0200

    Adiciona tela de seleção de pagamento

---
+++ src/main/java/br/edu/ifsp/PayNow/model/enuns/StatusDoPagamento.java
@@ -0,0 +1,8 @@
+package br.edu.ifsp.PayNow.model.enuns;
+
+/**
+ * Created by renato on 25/10/17.
+ */
+public enum StatusDoPagamento {
+    PENDENTE, VENCIDA, APROVADA, NAO_AUTORIZADO, PAGA_PARCIALMENTE;
+}
--- src/main/java/br/edu/ifsp/PayNow/model/enuns/StatusDoPagamentoBoleto.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifsp.PayNow.model.enuns;
-
-/**
- * Created by renato on 25/10/17.
- */
-public enum StatusDoPagamentoBoleto {
-    PENDENTE, VENCIDA, APROVADA;
-}
