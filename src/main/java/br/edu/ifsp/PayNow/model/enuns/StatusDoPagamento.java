0e2545e97fe3dea35508991d517f726ea7e5e4c5
renatosrib <renatosrib@gmail.com>
2017-11-06 21:04:21 -0200

    Evolui pagamento

--- src/main/java/br/edu/ifsp/PayNow/model/enuns/StatusDoPagamento.java
+++ src/main/java/br/edu/ifsp/PayNow/model/enuns/StatusDoPagamento.java
@@ -4,5 +4,15 @@ package br.edu.ifsp.PayNow.model.enuns;
  * Created by renato on 25/10/17.
  */
 public enum StatusDoPagamento {
-    PENDENTE, VENCIDA, APROVADA, NAO_AUTORIZADO, PAGA_PARCIALMENTE;
+    PENDENTE("PENDENTE"), VENCIDA("VENCIDA"), APROVADA("APROVADA"), NAO_AUTORIZADO("NÃO AUTORIZADO"), PAGA_PARCIALMENTE("PAGA PARCIALMENTE");
+    private final String text;
+
+    private StatusDoPagamento(final String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
 }
